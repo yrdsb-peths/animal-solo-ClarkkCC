bf33fb60067b65cc11b9cbc60ecb4e1e68fa17ef
ClarkkCC <Clark113008@gmail.com>
2025-04-30 14:17:00 -0400

    Add Apple

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,20 @@
 import greenfoot.*;
 
+/**
+ * The World the hero lives in
+ *
+ *@author Clark
+ *@version May 2025
+ */
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple(); 
+        addObject(apple, 300, 0);
     }
 }
+
