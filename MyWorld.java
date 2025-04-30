243464ae195924c8fe9879b1ed33265f09e809de
ClarkkCC <Clark113008@gmail.com>
2025-04-30 14:43:47 -0400

    ImportScore with LabelClass

--- MyWorld.java
+++ MyWorld.java
@@ -7,13 +7,34 @@ import greenfoot.*;
  *@version May 2025
  */
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    
+    /**
+     * Constructor for objects of class myWprld.
+     */
+    
     public MyWorld() {
         super(600, 400, 1);
         
+        //create the elephant object
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
+        //Create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
         createApple();
+    }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
     }
     
     /**
